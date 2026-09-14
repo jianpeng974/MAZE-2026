0251a63bd52d3e0e47e1039c3641db7fa57d8e94
jianpeng974 <jianpeng974@gmail.com>
2026-09-14 20:22:34 -0300

    Alinhamento
    teoria do alinhamento

---
+++ Alinhamento-teoria.ino
@@ -0,0 +1,14 @@
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+// Alinhamento
+// Toda vez que virar em uma curva vai para tras e se alinha com a parede
+// toda vez que vc virar vai ter que fazer o alinhamento, so que os movimentos de alinhamento nao sao considerados na lista de movimentos
+//ou seja , n precisa fazer somar uma posiçao no codigo
